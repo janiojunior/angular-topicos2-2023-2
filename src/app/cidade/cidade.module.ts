77fc75b4a3928df035b3d606801f0feb2f63e8d2
Janio <janiojunior@gmail.com>
2023-09-15 08:17:35 -0300

    CRUD de Cidades

--- src/app/cidade/cidade.module.ts
+++ src/app/cidade/cidade.module.ts
@@ -4,8 +4,16 @@ import { CommonModule } from '@angular/common';
 import { CidadeRoutingModule } from './cidade-routing.module';
 import { CidadeFormComponent } from './components/cidade-form/cidade-form.component';
 import { CidadeListComponent } from './components/cidade-list/cidade-list.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -15,7 +23,16 @@ import { MatSelectModule } from '@angular/material/select';
   imports: [
     CommonModule,
     CidadeRoutingModule,
-    MatSelectModule
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatTableModule
+
   ]
 })
 export class CidadeModule { }
--- src/app/cidade/components/cidade-list/cidade-list.component.ts
+++ src/app/cidade/components/cidade-list/cidade-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cidade } from 'src/app/models/cidade.model';
+import { CidadeService } from 'src/app/services/cidade.service';
 
 @Component({
   selector: 'app-cidade-list',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class CidadeListComponent {
 
+  tableColumns: string[] = ['id-column', 'nome-column', 'estado-column'];
+  cidades: Cidade[] = [];
+
+  constructor(private cidadeService: CidadeService) {}
+
+  ngOnInit(): void {
+    this.cidadeService.findAll().subscribe(data => {
+      this.cidades = data;
+    });
+  }
 }
